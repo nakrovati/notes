e0fa722f0cc13232b951d3271970620ed28c9345
Daniil Zhuravlev <nakrovati@outlook.com>
2024-02-25 21:03:51 +0400

    feat: add the abbility to log out of the account

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,4 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const user = useUser();
+
+async function handleLogout() {
+  try {
+    await $fetch("/api/logout", { method: "POST" });
+    await navigateTo("/");
+    user.value = null;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const items = [
+  [
+    {
+      label: "Account",
+      slot: "account",
+      disabled: true,
+    },
+  ],
+  [{ label: "Logout", click: () => handleLogout() }],
+];
+</script>
 
 <template>
   <header>
@@ -13,6 +36,20 @@
       <div v-else>Notes</div>
 
       <TheColorThemeToggle />
+
+      <UButton v-if="!user" variant="link" to="/login">Login</UButton>
+      <UDropdown v-else :items="items">
+        <UAvatar :alt="user.email"></UAvatar>
+
+        <template #account>
+          <div class="text-left">
+            <p>Signed in as</p>
+            <p class="truncate font-medium text-gray-900 dark:text-white">
+              {{ user.email }}
+            </p>
+          </div>
+        </template>
+      </UDropdown>
     </UContainer>
   </header>
 </template>
