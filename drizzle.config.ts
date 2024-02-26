3ff35b7206350e2aedacb527cae540dd1d023625
Daniil Zhuravlev <nakrovati@outlook.com>
2024-02-27 02:19:01 +0400

    fix: wrong path to the scheme in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/schema.ts",
+  schema: "./db/schema.ts",
   out: "./drizzle",
   driver: "turso",
   dbCredentials: {
