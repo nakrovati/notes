a6af258f84c82b1c7323998195d87c596f044893
Daniil Zhuravlev <nakrovati@outlook.com>
2024-02-27 01:50:55 +0400

    feat: add error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({
+  // eslint-disable-next-line vue/require-default-prop
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <div class="space-y-4 text-center">
+    <h1 class="mt-8 text-8xl font-bold">{{ error?.statusCode }}</h1>
+    <p>{{ error?.message }}</p>
+    <UButton to="/">Go home</UButton>
+  </div>
+</template>
